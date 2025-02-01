db0691d3ddaf00444d823887b9d05c558e4999e9
Naresh Bisht <nareshbishtasus@gmail.com>
2025-02-02 04:05:17 +0530

    Initial Commit

--- app/routes/search.tsx
+++ app/routes/search.tsx
@@ -2,9 +2,7 @@ import type {Route} from "./+types/search";
 
 clientLoader.hydrate = true;
 
-export async function clientLoader({ serverLoader, request }:Route.ClientLoaderArgs) {
-
-  return serverLoader();
+export async function clientLoader() {
 }
 
 export default function Search() {
